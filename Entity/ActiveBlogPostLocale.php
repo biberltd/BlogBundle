49afe4668fc6d14377faee91885730d9c1d58f34
Can Berkol <can.berkol@biberltd.com>
2016-01-29 14:30:00 +0200

    ActiveBlogPostLocale entity is tied to CoreBundle features.

--- Entity/ActiveBlogPostLocale.php
+++ Entity/ActiveBlogPostLocale.php
@@ -1,17 +1,20 @@
 <?php
 namespace BiberLtd\Bundle\BlogBundle\Entity;
+use BiberLtd\Bundle\CoreBundle\CoreEntity;
+use BiberLtd\Bundle\MultiLanguageSupportBundle\Entity\Language;
 use Doctrine\ORM\Mapping AS ORM;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="active_blog_post_locale", options={"charset":"utf8","collate":"utf8_turkish_ci"})
  */
-class ActiveBlogPostLocale
+class ActiveBlogPostLocale extends CoreEntity
 {
     /**
      * @ORM\Id
      * @ORM\ManyToOne(targetEntity="BiberLtd\Bundle\BlogBundle\Entity\BlogPost")
      * @ORM\JoinColumn(name="blog_post", referencedColumnName="id", nullable=false)
+     * @var BlogPost
      */
     private $blogPost;
 
@@ -19,6 +22,49 @@ class ActiveBlogPostLocale
      * @ORM\Id
      * @ORM\ManyToOne(targetEntity="BiberLtd\Bundle\MultiLanguageSupportBundle\Entity\Language")
      * @ORM\JoinColumn(name="language", referencedColumnName="id", nullable=false, onDelete="CASCADE")
+     * @var Language
      */
     private $language;
+
+    /**
+     * @return mixed
+     */
+    public function getLanguage(){
+        return $this->language;
+    }
+
+    /**
+     * @param \BiberLtd\Bundle\MultiLanguageSupportBundle\Entity\Language $language
+     *
+     * @return $this
+     */
+    public function setLanguage(Language $language){
+        if(!$this->setModified('language', $language)->isModified()){
+            return $this;
+        }
+        $this->language = $language;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBlogPost(){
+        return $this->blogPost;
+    }
+
+    /**
+     * @param \BiberLtd\Bundle\BlogBundle\Entity\BlogPost $blogPost
+     *
+     * @return $this
+     */
+    public function setBlogPost(BlogPost $blogPost){
+        if(!$this->setModified('blogPost', $blogPost)->isModified()){
+            return $this;
+        }
+        $this->blogPost = $blogPost;
+
+        return $this;
+    }
 }
