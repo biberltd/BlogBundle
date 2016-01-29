3f37c2e34bb1862c51dd357b0eb12a5d26e0865e
Can Berkol <can.berkol@biberltd.com>
2016-01-29 14:59:49 +0200

    ActiveBlogPostLocale getters/setters added.

--- Entity/ActiveBlogPostLocale.php
+++ Entity/ActiveBlogPostLocale.php
@@ -1,12 +1,13 @@
 <?php
 namespace BiberLtd\Bundle\BlogBundle\Entity;
+use BiberLtd\Bundle\CoreBundle\CoreEntity;
 use Doctrine\ORM\Mapping AS ORM;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="active_blog_post_locale", options={"charset":"utf8","collate":"utf8_turkish_ci"})
  */
-class ActiveBlogPostLocale
+class ActiveBlogPostLocale extends CoreEntity
 {
     /**
      * @ORM\Id
@@ -21,4 +22,76 @@ class ActiveBlogPostLocale
      * @ORM\JoinColumn(name="language", referencedColumnName="id", nullable=false, onDelete="CASCADE")
      */
     private $language;
+
+    /**
+     * @name        getBlogPost ()
+     *
+     * @author      Can Berkol
+     *
+     * @since       1.0.0
+     * @version     1.0.0
+     *
+     * @return      mixed
+     */
+    public function getBlogPost(){
+        return $this->blogPost;
+    }
+
+    /**
+     * @name        setBlogPost ()
+     *
+     * @author      Can Berkol
+     *
+     * @since       1.0.0
+     * @version     1.0.0
+     *
+     * @param       mixed $blogPost
+     *
+     * @return      $this
+     */
+    public function setBlogPost($blogPost){
+        if(!$this->setModified('blogPost', $blogPost)->isModified()){
+            return $this;
+        }
+        $this->blogPost = $blogPost;
+
+        return $this;
+    }
+
+    /**
+     * @name        getLanguage ()
+     *
+     * @author      Can Berkol
+     *
+     * @since       1.0.0
+     * @version     1.0.0
+     *
+     * @return      mixed
+     */
+    public function getLanguage(){
+        return $this->language;
+    }
+
+    /**
+     * @name        setLanguage ()
+     *
+     * @author      Can Berkol
+     *
+     * @since       1.0.0
+     * @version     1.0.0
+     *
+     * @param       mixed $language
+     *
+     * @return      $this
+     */
+    public function setLanguage($language){
+        if(!$this->setModified('language', $language)->isModified()){
+            return $this;
+        }
+        $this->language = $language;
+
+        return $this;
+    }
+
+
 }
