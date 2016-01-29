d6fc76616abd03e1f794f0776968e7d41f504de8
ozgedeniz <ozgedeniz.uysal@biberltd.com>
2016-01-29 14:39:36 +0200

    Active blog post locale entity added.

--- Entity/ActiveBlogPostLocale.php
+++ Entity/ActiveBlogPostLocale.php
@@ -6,14 +6,17 @@ use Doctrine\ORM\Mapping AS ORM;
 
 /**
  * @ORM\Entity
- * @ORM\Table(name="active_blog_post_locale", options={"charset":"utf8","collate":"utf8_turkish_ci"})
+ * @ORM\Table(
+ *     name="active_blog_post_locale",
+ *     options={"charset":"utf8","collate":"utf8_turkish_ci","engine":"innodb"}
+ * )
  */
 class ActiveBlogPostLocale extends CoreEntity
 {
     /**
      * @ORM\Id
      * @ORM\ManyToOne(targetEntity="BiberLtd\Bundle\BlogBundle\Entity\BlogPost")
-     * @ORM\JoinColumn(name="blog_post", referencedColumnName="id", nullable=false)
+     * @ORM\JoinColumn(name="blogPost", referencedColumnName="id", nullable=false, onDelete="CASCADE")
      * @var BlogPost
      */
     private $blogPost;
